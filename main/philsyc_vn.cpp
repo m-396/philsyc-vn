e2f771a191a597d1202d34839c5103e49ebdf10a
m-396 <matthew.esg@outlook.com>
2025-01-23 22:09:19 -0800

    new additions to philsyc_vn
    - switched format of functions
    - "cin.ignore(1000, '\n')" to "char ch" and "cin.get(ch)"
    - made invalid input loop until correct input
    - added new oneDialogue function for npc speech

--- main/philsyc_vn.cpp
+++ main/philsyc_vn.cpp
@@ -1,29 +1,11 @@
+// version 0.0.3
+
 #include<iostream>
 #include<iomanip>
 using namespace std;
 
 const int inventoryRow = 3;
 const int inventoryCol = 5;
-
-void introName();
-void continueGame();
-// maybe a function for where user inputs their name?
-void twoChoice(string prompt, string yes, string no);
-void playerInventory(const int inventory[][inventoryCol]);
-
-int main()
-{
-  int playerInv[inventoryRow][inventoryCol] = { {1, 2, 3, 4, 5}, {5, 4, 3, 2, 1}, {2, 2, 2, 2, 2} };
-
-  introName();
-  continueGame();
-
-  playerInventory(playerInv);
-  
-  twoChoice("You face a fork. Enter \"A\" to turn left, or \"B\" to turn right: ", "You turned left.", "You turned right.");
-  
-  return 0;
-}
 
 void introName()
 {
@@ -32,7 +14,7 @@ void introName()
     cout << "=";
   cout << endl;
   
-  cout << "== WELCOME TO CSUSM_VN ==" << endl;
+  cout << "== WELCOME TO PHILSYC_VN ==" << endl;
   
   for (int i = 1; i <= 25; i++)
     cout << "=";
@@ -41,26 +23,30 @@ void introName()
 
 void continueGame()
 {
+  char ch;
+  
   cout << "Press ENTER to continue: ";
-  cin.ignore(1000, '\n');
+  cin.get(ch);
   cout << endl;
 }
 
+// maybe a function for where user inputs their name?
 void twoChoice(string prompt, string yes, string no)
 {
   char option;
-  
-  cout << prompt;
-  cin >> option;
 
+  do
+    {
+      cout << prompt;
+      cin >> option;
+    } while (option != 'A' && option != 'a' && option != 'B' && option != 'b');
+      
   cout << endl;
   
   if (option == 'A' || option == 'a')
     cout << yes << endl;
   else if (option == 'B' || option == 'b')
     cout << no << endl;
-  else
-    cout << "Invalid!!!" << endl;
   cout << endl;
 } 
 
@@ -81,4 +67,29 @@ void playerInventory(const int inventory[][inventoryCol])
       cout << endl;
     }
   cout << endl;
+}
+
+void oneDialogue(string prompt)
+{
+  char ch;
+  cin.ignore(1000, '\n');
+  
+  cout << "\t" << prompt << " 'ENTER' to continue.";
+  cin.get(ch);
+}
+
+int main()
+{
+  int playerInv[inventoryRow][inventoryCol] = { {1, 2, 3, 4, 5}, {5, 4, 3, 2, 1}, {2, 2, 2, 2, 2} };
+
+  introName();
+  continueGame();
+
+  playerInventory(playerInv);
+  
+  twoChoice("You face a fork. Enter \"A\" to turn left, or \"B\" to turn right: ", ">> You turned left.", ">> You turned right.");
+  oneDialogue("What the hell? How did you end up here?");
+  
+  cout << endl << "everything worked." << endl;
+  return 0;
 }
